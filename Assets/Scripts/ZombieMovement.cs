d457fc26c3bb3cecc8dc57f58b20d793e09f193b
josephquested <josephquested@gmail.com>
2016-11-13 12:20:22 +1300

    intitial health bar commit

---
+++ Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieMovement : MonoBehaviour {
+	NavMeshAgent agent;
+
+	public Transform target;
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update ()
+	{
+		target = GameObject.FindWithTag("Player").transform;
+		if (target)
+		{
+			agent.destination = target.position;
+		}
+	}
+}
